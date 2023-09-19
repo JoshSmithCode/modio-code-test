d7e391d272eb77a8f54915245a7e47b1ec199ea1
Josh Smith <joshsmithcode@gmail.com>
2023-09-19 19:13:55 +1000

    Sprint 2 - Step 1

--- database/migrations/2023_09_14_060843_create_game_table.php
+++ database/migrations/2023_09_14_060843_create_game_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('game', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name')->index();
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('game');
+        Schema::dropIfExists('games');
     }
 };
