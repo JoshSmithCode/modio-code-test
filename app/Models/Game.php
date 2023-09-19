3299c04dcd2fc62b0c1fe6b6b97064783050ef01
Josh Smith <joshsmithcode@gmail.com>
2023-09-19 19:24:44 +1000

    Sprint 2 - Step 2 - Game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Game extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTimestamps;
+
+    public string $name;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
